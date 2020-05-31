851545811ce344cf3a3d493794d546382c2c0ec3
LeanneMK <ken17020@byui.edu>
2020-05-30 21:22:08 -0600

    9.22

--- scripts/main.js
+++ scripts/main.js
@@ -6,184 +6,184 @@ function toggleMenu() {
 document.querySelector('#menu').addEventListener('click', toggleMenu);
 
 
-////WEEK 03 EXamples
-//function doSomething() {
-//  console.log('Something Happened!');
-//}
-//addEventListener('click', doSomething);
+// ////WEEK 03 EXamples
+// function doSomething() {
+//     console.log('Something Happened!');
+// }
+// addEventListener('click', doSomething);
 
 
-//const clickParagraph = document.getElementById('click');
-//clickParagraph.addEventListener('click', () => console.log('click'));
-//clickParagraph.addEventListener('mousedown', () => console.log('down'));
-//clickParagraph.addEventListener('mouseup', () => console.log('up'));
+// const clickParagraph = document.getElementById('click');
+// clickParagraph.addEventListener('click', () => console.log('click'));
+// clickParagraph.addEventListener('mousedown', () => console.log('down'));
+// clickParagraph.addEventListener('mouseup', () => console.log('up'));
 
 
-//WEEK 04 Examples
-const form = document.forms['search'];
-form.addEventListener('submit', search, false);
+// //WEEK 04 Examples
+// const form = document.forms['search'];
+// form.addEventListener('submit', search, false);
 
-function search() {
-    alert(' Form Submitted');
-}
+// function search() {
+//     alert(' Form Submitted');
+// }
 
 //We can actually stop the form from being submitted to that URL altogether by using the preventDefault()
 //method that we saw in the last chapter. Add the following line to the search function:
 
-function search(event) {
-    alert('Form Submitted');
-    event.preventDefault();
-}
+//function search(event) {
+// alert('Form Submitted');
+// event.preventDefault();
+//}
 
 //We’ll also need a file called main.js that is saved in the same folder as the hero.html file.
 //In this file, let’s start off by assigning the form to a variable and then adding an event listener for when the form is submitted:
 
-const form = document.forms['hero'];
-form.addEventListener('submit', makeHero, false);
+//const form = document.forms['hero'];
+//form.addEventListener('submit', makeHero, false);
 
 //The event listener will invoke the makeHero() function when the form is submitted.
 //This function will return an object based on the information provided in the form.
 //Let’s implement that function by adding this code to main.js :
 
-function makeHero(event) {
-    event.preventDefault(); // prevent the form from being submitted
-    const hero = {}; // create an empty object
-    hero.name = form.heroName.value; // create a name property based on the input field's value
-    alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
-    return hero;
-}
+//function makeHero(event) {
+//   event.preventDefault(); // prevent the form from being submitted
+//   const hero = {}; // create an empty object
+//   hero.name = form.heroName.value; // create a name property based on the input field's value
+//   alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
+//   return hero;
+//}
 
 //We can then iterate over this collection using a for loop to see if each checkbox was checked.
 //Checkbox objects have a checked property that tells us if it has been checked or not. It is a boolean property,
 //so can only have the values true or false . The value property is used to set the name of the power that can be
 //used if the checkbox has been checked. Add the following code to the makeHero() function in main.js :
 
-hero.powers = [];
-for (let i = 0; i < form.powers.length; i++) {
-    if (form.powers[i].checked) {
-        hero.powers.push(form.powers[i].value);
-    }
-}
+//hero.powers = [];
+//for (let i = 0; i < form.powers.length; i++) {
+//  if (form.powers[i].checked) {
+//     hero.powers.push(form.powers[i].value);
+//  }
+//}
 
 //We can refactor this code to be much more succinct by using the array iterators we saw in Chapter 4. The following code will achieve the same result:
 
-hero.powers = [...form.powers].filter(box => box.checked).map(box => box.value);
+//hero.powers = [...form.powers].filter(box => box.checked).map(box => box.value);
 
 //It is also possible to implement custom form validation using JavaScript. For example, say we wanted to
 //exclude any superhero names that begin with an 'X'. This is not a standard form of validation, so we’d have
 //to write our own. Add this code to main.js to see an example of custom validation:
 
-form.addEventListener('submit', validate, false);
+//form.addEventListener('submit', validate, false);
 
-function validate(event) {
-    const firstLetter = form.heroName.value[0];
-    if (firstLetter.toUpperCase() === 'X') {
-        event.preventDefault();
-        alert('Your name is not allowed to start with X!');
-    }
-}
+//function validate(event) {
+// const firstLetter = form.heroName.value[0];
+//if (firstLetter.toUpperCase() === 'X') {
+//   event.preventDefault();
+//  alert('Your name is not allowed to start with X!');
+//  }
+//}
 
 //We can improve the usability of the form further by giving instant feedback, instead of waiting for the form to be submitted.
 //This can be achieved by adding the event listener directly to the input field that will fire when the user presses a key (using the keyup event).
 //The feedback can then be inserted inside the label element of the input field, along with a class of error for more direct feedback.
 //Add the following code to main.js :
 
-const label = form.querySelector('label');
-const error = document.createElement('div');
-error.classList.add('error');
-error.textContent = '! Your name is not allowed to start with X.';
-label.append(error);
+//**const label = form.querySelector('label');
+//**const error = document.createElement('div');
+// error.classList.add('error');
+// error.textContent = '! Your name is not allowed to start with X.';
+// label.append(error);
 
-function validateInline() {
-    const heroName = this.value.toUpperCase();
-    if (heroName.startsWith('X')) {
-        error.style.display = 'block';
-    } else {
-        error.style.display = 'none';
-    }
-}
+// function validateInline() {
+//     const heroName = this.value.toUpperCase();
+//     if (heroName.startsWith('X')) {
+//         error.style.display = 'block';
+//     } else {
+//         error.style.display = 'none';
+//     }
+// }
 
-//***WEEK 04 GROUP WORK */
-// Core Requirements
-const reset = document.getElementById('reset');
-const cells = document.querySelectorAll('.cell');
-const divBoard = document.forms.ticTac;
-const player1 = {
-    name: "player1",
-    sign: "X"
-};
-const player2 = {
-    name: "player2",
-    sign: "0"
-};
-let player = player1;
+// //***WEEK 04 GROUP WORK */
+// // Core Requirements
+// const reset = document.getElementById('reset');
+// const cells = document.querySelectorAll('.cell');
+// const divBoard = document.forms.ticTac;
+// const player1 = {
+//     name: "player1",
+//     sign: "X"
+// };
+// const player2 = {
+//     name: "player2",
+//     sign: "0"
+// };
+// let player = player1;
 
-document.getElementById("currentPlayer").innerHTML = player.name;
+// document.getElementById("currentPlayer").innerHTML = player.name;
 
-const getId = (e) => {
-    let cellName = e.target;
-    document.getElementById("currentPlayer").innerHTML = player.name;
-    cellName.innerHTML = player.sign;
-    if (player == player1) player = player2;
-    else player = player1;
-}
-// console.log(e.target);
+// const getId = (e) => {
+//     let cellName = e.target;
+//     document.getElementById("currentPlayer").innerHTML = player.name;
+//     cellName.innerHTML = player.sign;
+//     if (player == player1) player = player2;
+//     else player = player1;
+// }
+// // console.log(e.target);
 
-divBoard.addEventListener('click', getId);
-
-
-// 03
-// Add a reset button.
-// When touched the reset button should remove all of the marks from the board.
+// divBoard.addEventListener('click', getId);
 
 
-const resetBoard = (e) => {
-    e.preventDefault();
-    document.getElementById("currentPlayer").innerHTML = player1.name;
-    cells.forEach((c) => {
-        c.innerHTML = '';
-    });
-}
-
-reset.addEventListener('click', resetBoard, false);
+// // 03
+// // Add a reset button.
+// // When touched the reset button should remove all of the marks from the board.
 
 
-// WEEK 05
-import Hikes from './hikes.js';
+// const resetBoard = (e) => {
+//     e.preventDefault();
+//     document.getElementById("currentPlayer").innerHTML = player1.name;
+//     cells.forEach((c) => {
+//         c.innerHTML = '';
+//     });
+// }
 
-//create an array of hikes
-const hikeList = [{
-        name: "Bechler Falls",
-        imgSrc: "falls.jpg",
-        imgAlt: "Image of Bechler Falls",
-        distance: "3 miles",
-        difficulty: "Easy",
-        description: "Beautiful short hike along the Bechler river to Bechler Falls",
-        directions: "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead."
-    },
-    {
-        name: "Teton Canyon",
-        imgSrc: "falls.jpg",
-        imgAlt: "Image of Bechler Falls",
-        distance: "3 miles",
-        difficulty: "Easy",
-        description: "Beautiful short (or long) hike through Teton Canyon.",
-        directions: "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
-    },
-    {
-        name: "Denanda Falls",
-        imgSrc: "falls.jpg",
-        imgAlt: "Image of Bechler Falls",
-        distance: "7 miles",
-        difficulty: "Moderate",
-        description: "Beautiful hike through Bechler meadows river to Denanda Falls",
-        directions: "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
-    }
-];
+// reset.addEventListener('click', resetBoard, false);
 
-const myHike = new Hikes(hikeList);
 
-//on load grab the array and insert it into the page
-window.addEventListener("load", () => {
-    myHike.showHikeList();
-});
+// // WEEK 05
+// import Hikes from './hikes.js';
+
+// //create an array of hikes
+// const hikeList = [{
+//         name: "Bechler Falls",
+//         imgSrc: "falls.jpg",
+//         imgAlt: "Image of Bechler Falls",
+//         distance: "3 miles",
+//         difficulty: "Easy",
+//         description: "Beautiful short hike along the Bechler river to Bechler Falls",
+//         directions: "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead."
+//     },
+//     {
+//         name: "Teton Canyon",
+//         imgSrc: "falls.jpg",
+//         imgAlt: "Image of Bechler Falls",
+//         distance: "3 miles",
+//         difficulty: "Easy",
+//         description: "Beautiful short (or long) hike through Teton Canyon.",
+//         directions: "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
+//     },
+//     {
+//         name: "Denanda Falls",
+//         imgSrc: "falls.jpg",
+//         imgAlt: "Image of Bechler Falls",
+//         distance: "7 miles",
+//         difficulty: "Moderate",
+//         description: "Beautiful hike through Bechler meadows river to Denanda Falls",
+//         directions: "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
+//     }
+// ];
+
+// const myHike = new Hikes(hikeList);
+
+// //on load grab the array and insert it into the page
+// window.addEventListener("load", () => {
+//     myHike.showHikeList();
+// });**//
